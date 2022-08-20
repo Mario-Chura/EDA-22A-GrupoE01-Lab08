2fd8da444beaced3e33c24ad25b45331c774d742
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 09:57:38 -0500

    metod equals implement

--- Ejercicio_03_BSF/Vertex.java
+++ Ejercicio_03_BSF/Vertex.java
@@ -10,4 +10,15 @@ public class Vertex<E> {
         listAdj = new ListLinked<Edge<E>>();
     }
 
+    //Metodos
+    public boolean equals(Object o) {
+        if (o instanceof Vertex<?>) {
+            Vertex<E> v = (Vertex<E>) o;
+            return this.data.equals(v.data);
+        }
+        return false;
+    }
+    
+
+
 }
