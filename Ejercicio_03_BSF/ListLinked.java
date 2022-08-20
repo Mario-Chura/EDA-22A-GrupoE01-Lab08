4e026dcfb605647c2c18ab1633f9fa7cf5c3275d
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 10:20:54 -0500

    isEmpty y toString: metodos implementados

--- Ejercicio_03_BSF/ListLinked.java
+++ Ejercicio_03_BSF/ListLinked.java
@@ -6,5 +6,19 @@ public class ListLinked<T> {
 	public ListLinked() {
 		this.first = null;
 	}
-	
+
+	//Metodos
+	public boolean isEmpty() {
+		return this.first == null;
+	}
+
+	public String toString() {
+		String r = "";
+		Node<T> aux = this.first;
+			while(aux != null) {
+				r = r + aux.data; //voy concatenando la información
+				aux = aux.next;
+			}
+		return r; 
+	}
 }
