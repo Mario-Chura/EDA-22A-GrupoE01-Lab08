ca5d437edc1bb434826aba59d1ac0ca2f34a15ed
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 13:28:06 -0500

    atributos agregados

---
+++ Ejercicio_03_Dijkstra/Graph.java
@@ -0,0 +1,14 @@
+
+public class Graph {
+    //Atributos    
+    private int n; //número de punto fijo n
+    private int numberOfEdges;
+    private double[] distance;
+    private String[] path;
+    private ArrayList<String> Vertax; //conjunto de vértices Vertax
+    private static int[][] edges; //conjunto de aristas
+    private boolean[] isVisited; //si ha sido visitado es el marcador visitado
+
+    
+
+}
