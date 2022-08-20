c60417df3d4befc3465286b7c03286e3cc7c1506
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 12:09:03 -0500

    clase implementada

---
+++ Ejercicio_03_DFS/Vertex.java
@@ -0,0 +1,24 @@
+public class Vertex<E> {
+	//Atributos
+	protected E data;
+	protected ListLinked<Edge<E>> listAdj;
+	protected int label; // "0=inexplorado" , "1 = visitado"
+
+	//Constructor
+	public Vertex(E data) {
+		this.data = data;
+		listAdj = new ListLinked<Edge<E>>();
+	}
+
+	public boolean equals(Object o) {
+		if (o instanceof Vertex<?>) {
+			Vertex<E> v = (Vertex<E>) o;
+			return this.data.equals(v.data);
+		}
+		return false;
+	}
+
+	public String toString() { //Muestra el dato y la lista de adyacencia del vertice
+		return this.data + " --> " + this.listAdj.toString() + "\n";
+	}
+}
