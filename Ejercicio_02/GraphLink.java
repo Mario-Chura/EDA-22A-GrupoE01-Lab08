e8840ee25e56377ccf696c8a6a7392f109a12ea2
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 01:02:12 -0500

    constructor agregado

--- Ejercicio_02/GraphLink.java
+++ Ejercicio_02/GraphLink.java
@@ -2,5 +2,10 @@ public class GraphLink <E>{
 	//Atributo
 	protected ListLinked<Vertex<E>> listVertex; //Lista enlazada de vertices
 
-	
+	//Constructor
+
+	//Crea una lista vacia	
+	public GraphLink() { 
+		listVertex = new ListLinked<Vertex<E>>();//Se inicializa la lista, generando una lista vacìa de aristas
+	}
 }
