c61ea0f2bb96ff5ca9b2089075abdff8be7091cb
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 11:09:31 -0500

    insertVertex metodo implementado

--- Ejercicio_03_BSF/GraphLink.java
+++ Ejercicio_03_BSF/GraphLink.java
@@ -6,7 +6,15 @@ public class GraphLink<E> {
 	public GraphLink() {
 		listVertex = new ListLinked<Vertex<E>>();
 	}
-	
+
 	//Metodos
+	public void insertVertex(E data) { //Recibe de argumento la informacion que va almacenar el vertice
+		Vertex<E> nuevo = new Vertex<E>(data); //Se declara e inicializa nuevo vertice
+		if (this.listVertex.search(nuevo) != null) { //Si es distinto de nulo, significa que ese vértice ya esta dentro de la lista
+			System.out.println("Vertice ya fue insertado anteriormente ... ");
+		}
+		this.listVertex.insertFirst(nuevo);//En caso de no encontrarse, se inserta en la lista, el vértice
+		System.out.println("vertice " + nuevo + " insertado");
+	}
 
 }
