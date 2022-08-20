e83af03d8e0401678e5770253d73574fab1e8863
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 12:23:08 -0500

    insertVertex implementado

--- Ejercicio_03_DFS/GraphLink.java
+++ Ejercicio_03_DFS/GraphLink.java
@@ -6,6 +6,15 @@ public class GraphLink<E> {
 	public GraphLink() { //Crea una lista vacía 	
 		listVertex = new ListLinked<Vertex<E>>();//Se declara una lista vacìa de aristas
 	}
-	
+
+	//Metodos
+	public void insertVertex(E data) {
+		Vertex<E> nuevo = new Vertex<E>(data);
+		if (this.listVertex.search(nuevo) != null) {
+			System.out.println("Vertice ya fue insertado anteriormente ... ");
+		}
+		this.listVertex.insertFirst(nuevo);
+		System.out.println("vertice " + nuevo + " insertado");
+	}
 
 }
