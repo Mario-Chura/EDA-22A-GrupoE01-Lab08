b73370d1a18f5b606401d1a4b8b86f3c44ae0a9f
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 00:11:36 -0500

    agregando comentarios

--- Ejercicio_02/ListLinked.java
+++ Ejercicio_02/ListLinked.java
@@ -9,16 +9,18 @@ public class ListLinked<T> {
 
 	//Metodos
 
+	//Metodo Booleano que define la existencia del grafo
 	public boolean isEmpty() {
 		return this.first == null;	
 	}
 
+	//Para mostrar la informacion del Grafo
 	public String toString() {
 		String r = "";
 		Node<T> aux = this.first;
-			while(aux != null) {
-				r = r + aux.data; //voy concatenando la información
-				aux = aux.next;
+			while(aux != null) { //Mientras existan los nodos
+				r = r + aux.data; //la informacion se va concatenando 
+				aux = aux.next; //aux toma el valor del sgte Nodo
 			}
 			return r; 
 	}
