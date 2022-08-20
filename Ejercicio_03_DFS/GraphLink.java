45c42b6a13450e215465cd817c223bce32506dc0
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-20 12:13:50 -0500

    constructor y atributo

---
+++ Ejercicio_03_DFS/GraphLink.java
@@ -0,0 +1,11 @@
+public class GraphLink<E> {
+	//Atributo
+	protected ListLinked<Vertex<E>> listVertex;
+
+	//Constructor
+	public GraphLink() { //Crea una lista vacía 	
+		listVertex = new ListLinked<Vertex<E>>();//Se declara una lista vacìa de aristas
+	}
+	
+
+}
